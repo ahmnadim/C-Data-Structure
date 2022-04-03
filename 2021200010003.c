20fa0ee318fe62e966f70d7ab76ad09d37f4b85b
ahmnadim <ahmnadim.2018@gmail.com>
2022-04-03 21:20:37 +0600

    fix

--- 2021200010003.c
+++ 2021200010003.c
@@ -31,140 +31,6 @@ int insertItemBegin(int item)
     return SUCCESS_VALUE ;
 }
 
-int insertItemLast(int item)
-{
-    struct listNode * newNode, *temp = list;
-    newNode = (struct listNode*) malloc (sizeof(struct listNode)) ;
-
-    newNode->item = item;
-    newNode->next = NULL;
-
-	if (temp == NULL) {
-		list = newNode;
-	} else {
-		while (temp->next != NULL){
-            temp = temp->next;
-		}
-		temp->next = newNode;
-	}
-	length++;
-    return SUCCESS_VALUE;
-}
-
-int insertItemAT(int position, int item)
-{
-    if(position < 0 || position > length){
-        return NULL_VALUE;
-    }else if(position == 0){
-        insertItemBegin(item);
-    }else if(position == length){
-        insertItemLast(item);
-    }else{
-        struct listNode * newNode, *temp, *prev, *next;
-        newNode = (struct listNode*) malloc (sizeof(struct listNode)) ;
-        temp = list;
-        newNode->item = item;
-        newNode->next = NULL;
-         for (int i = 1; i != position; i++){
-            temp = temp -> next;
-         }
-		newNode -> next = temp -> next;
-		temp -> next = newNode;
-
-        length++;
-        return SUCCESS_VALUE;
-    }
-
-}
-
-int NumOfItems(int item){
-    struct listNode *temp;
-    int count = 0;
-    temp = list ;
-    while (temp != NULL)
-    {
-        if (temp->item == item){
-            count++;
-        }
-        temp = temp->next ;
-    }
-    return count;
-}
-
-int deleteALLOccurrence(int item){
-    struct listNode *temp = list, *prev;
-    if(temp == NULL) return NULL_VALUE;
-
-    while (temp != NULL && temp->item == item){
-        list = temp->next; // Changed head
-        free(temp); // free old head
-        temp = list; // Change Temp
-    }
-
-    while (temp != NULL){
-        while (temp != NULL && temp->item != item){
-            prev = temp;
-            temp = temp->next;
-        }
-
-        if (temp == NULL) return NULL_VALUE;
-        prev->next = temp->next;
-        free(temp); // Free memory
-        temp = prev->next;
-    }
-    return SUCCESS_VALUE ;
-}
-
-int deleteLastOccurrence(int item){
-    struct listNode * temp = list;
-    if(temp == NULL) return NULL_VALUE;
-    struct listNode * prev = NULL,* curr = NULL;
-
-      while(temp){
-        if(temp->next && temp->next->item == item){
-          prev = temp;
-          curr = temp->next;
-        }
-        temp = temp->next;
-      }
-      if(prev){
-        prev->next = curr->next;
-      }else if(temp->item == item){
-        temp = temp->next;
-      }
-      length--;
-      return SUCCESS_VALUE ;
-}
-
-int deleteFirstItem(){
-    struct listNode *temp = list;
-    if(temp = NULL){
-        return NULL_VALUE;
-    }
-    list = list->next;
-    free(temp);
-    length--;
-    return SUCCESS_VALUE;
-}
-
-int deletelastItem(){
-    struct listNode *temp = list, *lastNode;
-    if(temp == NULL){
-        return NULL_VALUE;
-    }
-    if(temp->next == NULL){
-        deleteFirstItem();
-    }
-    while(temp-> next->next != NULL){
-        temp = temp->next;
-    }
-    lastNode = temp->next;
-    temp->next = NULL;
-    free(lastNode);
-    length--;
-    return SUCCESS_VALUE;
-}
-
 int searchItem(int item)
 {
     struct listNode *temp ;
@@ -195,6 +61,7 @@ void PrintALL()
 {
     struct listNode *temp;
     temp = list ; //start at the beginning
+    printf("All items of the list: ");
     while (temp != 0)
     {
         printf("%d ",temp->item);
@@ -230,8 +97,162 @@ int deleteFirstOccurrence(int item)
 }
 
 
+// ASSIGNMENTS STARTS FROM HERE...
+// =================================================================
+// =================================================================
 
+// Insert item at the last position of the list...
+int insertItemLast(int item)
+{
+    struct listNode * newNode, *temp = list;
+    newNode = (struct listNode*) malloc (sizeof(struct listNode)) ;
 
+    newNode->item = item;
+    newNode->next = NULL;
+
+	if (temp == NULL) {
+		list = newNode;
+	} else {
+		while (temp->next != NULL){
+            temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+	length++;
+    return SUCCESS_VALUE;
+}
+
+// Insert item at the given position of the list...
+int insertItemAT(int position, int item)
+{
+    if(position < 0 || position > length){
+        return NULL_VALUE;
+    }else if(position == 0){
+        insertItemBegin(item);
+    }else if(position == length){
+        insertItemLast(item);
+    }else{
+        struct listNode * newNode, *temp, *prev, *next;
+        newNode = (struct listNode*) malloc (sizeof(struct listNode)) ;
+        temp = list;
+        newNode->item = item;
+        newNode->next = NULL;
+         for (int i = 1; i != position; i++){
+            temp = temp -> next;
+         }
+		newNode -> next = temp -> next;
+		temp -> next = newNode;
+
+        length++;
+        return SUCCESS_VALUE;
+    }
+
+}
+
+// return the number of the occurrence of the given item
+int NumOfItems(int item){
+    struct listNode *temp;
+    int count = 0;
+    temp = list ;
+    while (temp != NULL)
+    {
+        if (temp->item == item){
+            count++;
+        }
+        temp = temp->next ;
+    }
+    return count;
+}
+
+// Delete all the occurrence of the given item in the list
+// with preserving order
+
+int deleteALLOccurrence(int item){
+    struct listNode *prev, *cur;
+
+    while (list != NULL && list->item == item){
+        prev = list;
+        list = list->next;
+        free(prev);
+        length--;
+    }
+
+    prev = NULL;
+    cur  = list;
+
+    while (cur != NULL){
+        if (cur->item == item){
+            if (prev != NULL){
+                prev->next = cur->next;
+            }
+            free(cur);
+            cur = prev->next;
+            length--;
+        }
+        else{
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return SUCCESS_VALUE ;
+}
+
+// Delete the last occurrence of the given item in the list
+int deleteLastOccurrence(int item){
+    struct listNode * temp = list;
+    if(temp == NULL) return NULL_VALUE;
+    struct listNode * prev = NULL,* curr = NULL;
+
+      while(temp){
+        if(temp->next && temp->next->item == item){
+          prev = temp;
+          curr = temp->next;
+        }
+        temp = temp->next;
+      }
+      if(prev){
+        prev->next = curr->next;
+      }else if(temp->item == item){
+        temp = temp->next;
+      }
+      length--;
+      return SUCCESS_VALUE ;
+}
+
+// delete the first item of the list
+int deleteFirstItem(){
+    struct listNode *temp = list;
+    if(temp = NULL){
+        return NULL_VALUE;
+    }
+    list = list->next;
+    free(temp);
+    length--;
+    return SUCCESS_VALUE;
+}
+
+// delete the last item of the list
+int deletelastItem(){
+    struct listNode *temp = list, *lastNode;
+    if(temp == NULL){
+        return NULL_VALUE;
+    }
+    if(temp->next == NULL){
+        deleteFirstItem();
+    }
+    while(temp-> next->next != NULL){
+        temp = temp->next;
+    }
+    lastNode = temp->next;
+    temp->next = NULL;
+    free(lastNode);
+    length--;
+    return SUCCESS_VALUE;
+}
+
+// ASSIGNMENTS FUNCTIONS ENDS.......
+// ===========================================
+// ===========================================
 
 
 int main()
@@ -241,75 +262,75 @@ int main()
     int s;
     initializeList();
     while(1){
-        printf("what you want: ");
         scanf("%c",&inp1);
-
 
         switch(inp1)
         {
-        case 'U':
-            scanf("%d",&inp2);
-            s=insertItemBegin(inp2);
-            break;
+            case 'I':
+                scanf("%d",&inp2);
+                s=insertItemBegin(inp2);
+                break;
 
-        case 'P':
-            scanf("%d",&inp2);
-            s=insertItemLast(inp2);
-            break;
+            case 'G':
+                scanf("%d",&inp2);
+                s=GetItem(inp2);
+                printf("You are trying to get: %d\n",s);
+                break;
 
-        case 'I':
-            scanf("%d %d",&inp2, &pos);
-            s=insertItemAT(pos, inp2);
-            break;
+            case 'S':
+                scanf("%d",&inp2);
+                printf("Address: %d\n",searchItem(inp2));
+                break;
 
-        case 'N':
-            scanf("%d", &inp2);
-            s=NumOfItems(inp2);
-            printf("%d\n", s);
-            break;
+            case 'R':
+                scanf("%d",&inp2);
+                s=deleteFirstOccurrence(inp2);
+                break;
 
-        case 'D':
-            scanf("%d", &inp2);
-            s=deleteALLOccurrence(inp2);
-            break;
+            case 'P':
+                PrintALL();
+                break;
 
-        case 'O':
-            scanf("%d", &inp2);
-            s=deleteLastOccurrence(inp2);
-            break;
+            case 'L':
+                printf("Length: %d\n",length);
+                break;
 
-        case 'F':
-            s=deleteFirstItem();
-            break;
 
-        case 'X':
-            s=deletelastItem();
-            break;
+            case 'A':
+                scanf("%d",&inp2);
+                s=insertItemLast(inp2);
+                break;
 
-        case 'G':
-            scanf("%d",&inp2);
-            s=GetItem(inp2);
-            printf("%d\n",s);
-            break;
+            case 'B':
+                scanf("%d %d",&inp2, &pos);
+                s=insertItemAT(pos, inp2);
+                break;
 
-        case 'S':
-            scanf("%d",&inp2);
-            printf("Address: %d\n",searchItem(inp2));
-            break;
+            case 'C':
+                scanf("%d", &inp2);
+                s=NumOfItems(inp2);
+                printf("Total number of %d in the list is: %d\n", inp2, s);
+                break;
 
-        case 'R'://delete the item of first occurrence
-            scanf("%d",&inp2);
-            s=deleteFirstOccurrence(inp2);
-            break;
+            case 'D':
+                scanf("%d", &inp2);
+                s=deleteALLOccurrence(inp2);
+                break;
 
-        case 'E':
-            PrintALL();
-            break;
+            case 'E':
+                scanf("%d", &inp2);
+                s=deleteLastOccurrence(inp2);
+                break;
 
-        case 'L':
-            printf("Length: %d\n",length);
-            break;
+            case 'F':
+                s=deleteFirstItem();
+                break;
+
+            case 'Z':
+                s=deletelastItem();
+                break;
         }
+
         if(s==NULL_VALUE) break;
     }
 
